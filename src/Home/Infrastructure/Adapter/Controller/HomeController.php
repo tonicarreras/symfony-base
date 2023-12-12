d7f2147235b8253e05c214a799d4c9fec716847f
tonicarreras <antoniocarrerasalarcon@gmail.com>
2023-12-12 10:35:58 +0100

    add home details

--- src/Home/Infrastructure/Adapter/Controller/HomeController.php
+++ src/Home/Infrastructure/Adapter/Controller/HomeController.php
@@ -4,16 +4,30 @@ declare(strict_types=1);
 
 namespace Home\Infrastructure\Adapter\Controller;
 
+use Composer\InstalledVersions;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Routing\Attribute\Route;
 
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'health_check', methods: ['GET'])]
+    #[Route('/', name: 'home', methods: ['GET'])]
     public function __invoke(): JsonResponse
     {
-        return $this->json(['status' => 'ok']);
+        $kernel = Kernel::class;
+        $symfonyVersion = $kernel::VERSION;
+
+        $package = InstalledVersions::getRootPackage();
+        $currentTime = new \DateTimeImmutable();
+
+        return $this->json([
+            'project_name' => $package['name'],
+            'project_version' => $package['version'],
+            'symfony_version' => $symfonyVersion,
+            'php_version' => PHP_VERSION,
+            'currentTime' => $currentTime->format('Y-m-d H:i:s')
+        ]);
     }
 }
